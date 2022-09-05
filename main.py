9afa465185d0783979af9dead8f170ca12e4e0f6
Ivan Hart <ihart231745@gmail.com>
2022-09-05 13:40:21 -0500

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+from turtle import Screen
+import time
+
+def exit():
+    global running
+    running = False
+
+print("Deleting system in 10")
+time.sleep(1)
+for i in range(9, 0, -1):
+    print(i)
+    time.sleep(1)
+
+running = True
+
+win = Screen()
+screenTk = win.getcanvas().winfo_toplevel()
+screenTk.attributes("-fullscreen", True)
+win.bgcolor("#000")
+time.sleep(1.5)
+win.bgpic("Bsodwindows10.png")
+win.listen()
+
+while running:
+    win.onkey(exit, 'p')
